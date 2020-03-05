2c908c27cf4633b346cf2b602015e5718028f0c5
kaazuk1987 <kaazuk1987@gmail.com>
2020-03-05 20:06:02 +0100

    owl carousel brakuje skryptu auto|infinite

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(".panie").css("margin-right","50px");
 
 
 $(".panie").mouseenter(function(){
-$(".panie").css("margin-right","-160px");
+$(".panie").css("margin-right","-162px");
 $(".panie > .d-kolo").show();
 
 
