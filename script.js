0e3c51d8d293dca2267235062fd38afb592065be
kaazuk1987 <kaazuk1987@gmail.com>
2020-03-06 08:36:29 +0100

    autoplay w karuzeli dziala ale tylko play stop nie widzi przestawionych pol obiektu jak loop np. czyli lipa bo karuzela ma 4 elementy

--- script.js
+++ script.js
@@ -14,7 +14,24 @@ $(".panie > .d-kolo").hide();
 $(".panie").css("margin-right","50px");
 });
 
-
+//karuzela
+$(".owl-carousel").owlCarousel();
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+    items:4,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[1000])
+    owl.trigger('play.owl.loop')
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
 
 });
 
