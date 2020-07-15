6c720894c1dfc72cddca987acec34d52efc6afdf
uk <uk@debian.abc>
2020-07-15 13:01:48 +0200

    moze w koncu

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(".panie").css("margin-right","50px");
 
 
 $(".panie").mouseenter(function(){
-$(".panie").css("margin-right","-161px");
+$(".panie").css("margin-right","-162px");
 $(".panie > .d-kolo").show();
 
 
